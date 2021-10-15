2f73af00dad9acc0fac3c2b694e3818ffa32b980
sernamar <sernamar@protonmail.com>
2021-10-15 16:31:20 +0200

    Make a simple example where processes are schedule in creation order

--- src/os-scheduler.lisp
+++ src/os-scheduler.lisp
@@ -3,6 +3,7 @@
 (declaim (optimize safety)) ; necesary to check the type of the slot "state"
 
 (defvar *pid* 0)
+(defconstant +max-run-time+ 10)
 
 (deftype process-state () '(member :running :ready :done :waiting))
 
@@ -27,4 +28,16 @@
 (defun create-processes (&optional (number-of-processes 2))
   (loop :repeat number-of-processes
         :do (incf *pid*)
-        :collect (make-process :pid *pid*)))
+        :collect (make-process :pid *pid* :run-time (1+ (random +max-run-time+)))))
+
+(defun print-workload (workload)
+  (dolist (process workload)
+    (format t "pid: ~d, state: ~a, run-time: ~d, arrival time: ~d~%"
+            (:pid process) (:state process) (:run-time process) (:arrival-time process))))
+
+(defun simulate (&optional (number-of-processes 2))
+  (let ((workload (create-processes number-of-processes)))
+    (print-workload workload)
+    (dolist (process workload)
+      (loop :for i :from 1 :to (:run-time process)
+            :do (format t "Process ~d: ~d~%" (:pid process) i)))))
